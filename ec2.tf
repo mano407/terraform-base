5f01398e3ffc9b2c6482d579e9cac927793138c5
manohar <manohar0407@gmail.com>
2025-03-19 12:48:19 +0530

    terraform base code

---
+++ ec2.tf
@@ -0,0 +1,7 @@
+resource "aws_instance" "web" {
+  ami           = "var.ami-id"
+  instance_type = "var.instance_type"
+    tags = {
+    Name = "HelloWorld"
+  }
+}
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+ami           = "ami-0e1bed4f06a3b463d"
+instance_type = "t2.micro"
